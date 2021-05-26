6534b66ac1efef25575ee3e4b8aeda3a74f238ba
Anwesha Tomar <atomar19@gwu.edu>
2021-05-26 12:46:11 -0400

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,56 @@
+Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    
+"""
+List of companies: Microsoft, Bloomberg, Amazon, Facebook, Google,Apple, Spotify
+
+Day 2-
+
+Given a string s, find the length of the longest substring without repeating characters.
+
+ 
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+Example 4:
+
+Input: s = ""
+Output: 0
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+"""
+
+Solution:
+  
+  class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        d={}
+        maxi=0
+        pre=0
+        for i in range(len(s)):
+            if s[i] not in d:
+                d[s[i]]=[i]
+                maxi=max(maxi,i-pre+1)
+            else:
+                key=d[s[i]][-1]
+                if key>=pre:
+                    pre=key+1
+                maxi=max(maxi,i-pre+1)
+                d[s[i]].append(i)
+        return maxi
